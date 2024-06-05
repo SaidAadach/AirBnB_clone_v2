9a3ae082d7b8b2e265c3121e9953deafb86be485
saidaadach <saidaadach@gmail.com>
2024-06-05 13:05:33 +0000

    Up

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application
-"""
-
+"""Démarre une application web Flask"""
 from flask import Flask
+
 app = Flask(__name__)
 
+@app.route('/airbnb-onepage/', strict_slashes=False)
+def hello_hbnb():
+    """Retourne une chaîne de caractères à la route /airbnb-onepage/"""
+    return "Hello HBNB!"
 
-@app.route('/', strict_slashes=False)
-def index():
-    """returns Hello HBNB!"""
-    return 'Hello HBNB!'
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
